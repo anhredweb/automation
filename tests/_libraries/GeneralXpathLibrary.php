309374328fb1e227868a0fa760faed066efad522
Tu Vuong Huynh Thanh <vuonghuynhthanhtu@deltavn.vn>
2017-12-25 10:00:01 +0700

    update code

--- tests/_libraries/GeneralXpathLibrary.php
+++ tests/_libraries/GeneralXpathLibrary.php
@@ -24,7 +24,7 @@ class GeneralXpathLibrary
 	
 	public static $nationalIdScoring = '//div[@section_index="4"]//div[@data-node-id="pyCaseSummary"]//div[contains(@class, "content-item content-layout item-3")]//div[contains(@class, "content-item content-field item-4")]//div[contains(@class, "content-inner ")]//div[contains(@class, "dataValueRead")]//span';
 	
-	public static $applicationId     = '(//div[@data-node-id="pyCaseContainer"]//div[@data-node-id="pyCaseSummary"]//div[contains(@class, "content-item content-layout item-3")]//div[contains(@class, "content-item content-field item-3")]//div[contains(@class, "content-inner ")]//div[contains(@class, "dataValueRead")]//span)[1]';
+	public static $applicationId     = '(//div[@data-node-id="pyCaseContainer"]//div[@data-node-id="pyCaseSummary"]//div[contains(@data-ui-meta, "LoanApp.ApplicationID")])[1]';
 	
 	public static $totalScore        = '(//div[@data-node-id="ScoringInfo"]//div[@swp=".pyDescription,.StrategyResult.Result"])[3]//div[contains(@class, "content-item content-field item-2")]//label[contains(@for, "Score")]//following-sibling::div[contains(@class, "dataValueRead")]//span';
 	
