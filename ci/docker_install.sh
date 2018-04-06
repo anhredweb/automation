19b8f8297464446a4d45b067bde4955000c0524f
tuvuong@asiantechhub.com <tuvuong@asiantechhub.com>
2018-04-06 18:11:51 +0700

    update code

--- ci/docker_install.sh
+++ ci/docker_install.sh
@@ -6,7 +6,7 @@
 set -xe
 
 apt-get update -yqq
-apt-get install zip unzip openjdk-8-jre
+apt-get install zip unzip
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php composer-setup.php
 php -r "unlink('composer-setup.php');"
