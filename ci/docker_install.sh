2cf3975f6742654c13e5f52a2111d9c4e33c2a20
tuvuong@asiantechhub.com <tuvuong@asiantechhub.com>
2018-04-06 16:54:24 +0700

    update code

--- ci/docker_install.sh
+++ ci/docker_install.sh
@@ -6,6 +6,7 @@
 set -xe
 
 apt-get update -yqq
+composer install
 
 # Install phpunit, the tool that we will use for testing
 curl --location --output /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.phar
