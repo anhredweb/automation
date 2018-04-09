c8a1a46190276658b01c491c8e1dba44bd68ebba
tuvuong@asiantechhub.com <tuvuong@asiantechhub.com>
2018-04-09 14:59:15 +0700

    update code

--- ci/script.sh
+++ ci/script.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -ev
 
-java -jar -Dwebdriver.chrome.driver="chrome_driver/chromedriver" "selenium-server-standalone.jar"
-vendor/bin/codecept run tests/acceptance/pl/initPLCest.php --steps
+php vendor/bin/codecept run tests/acceptance/pl/initPLCest.php --steps
