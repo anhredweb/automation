5ffbd1b8183461af245d2b613f205fc1dbb77c07
tuvuong@asiantechhub.com <tuvuong@asiantechhub.com>
2018-04-09 16:26:38 +0700

    update code

--- ci/setup.sh
+++ ci/setup.sh
@@ -41,13 +41,13 @@ else
 	sudo a2ensite default.conf
 	sudo service apache2 restart
 
+	composer install
+
+	npm install
+
 	sh -e /etc/init.d/xvfb start
 	sleep 3
 	sudo apt-get install fluxbox -y --force-yes
 	fluxbox &
 	sleep 3
-
-	composer install
-
-	npm install
 fi
