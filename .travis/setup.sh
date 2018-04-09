4d4d69ca36ac82d47575b212c36380999f458d73
tuvuong@asiantechhub.com <tuvuong@asiantechhub.com>
2018-04-09 10:20:19 +0700

    update code

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
 set -ev
-
-apt-get update -yqq
-apt-get install zip unzip
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php composer-setup.php
-php -r "unlink('composer-setup.php');"
-php composer.phar install
