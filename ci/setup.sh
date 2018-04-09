dca9e402f7e5d3c5ae4b7dd3dd01efaf12010b67
tuvuong@asiantechhub.com <tuvuong@asiantechhub.com>
2018-04-09 17:39:17 +0700

    update code

--- ci/setup.sh
+++ ci/setup.sh
@@ -4,38 +4,22 @@ set -ev
 npm install -g gulp-cli
 
 # forcing localhost to be the 1st alias of 127.0.0.1 in /etc/hosts (https://github.com/seleniumhq/selenium/issues/2074)
-sudo sed -i '1s/^/127.0.0.1 localhost\n/' /etc/hosts
-
+sudo sed -i '1s/^/127.0.0.1 localhost\n/' /etc/hosts # forcing localhost to be the 1st alias of 127.0.0.1 in /etc/hosts (https://github.com/seleniumhq/selenium/issues/2074)
 sudo apt-get update -qq
-	sudo apt-get install --yes --force-yes apache2 libapache2-mod-fastcgi oracle-java8-installer oracle-java8-set-default fluxbox -y --force-yes
-	sudo mkdir $(pwd)/.run
-
-phpversionname="$(phpenv version-name)"
-file="/home/$USER/.phpenv/versions/$phpversionname/etc/php-fpm.conf"
-cp /home/$USER/.phpenv/versions/$phpversionname/etc/php-fpm.conf.default /home/$USER/.phpenv/versions/$phpversionname/etc/php-fpm.conf
-if [ -f /home/$USER/.phpenv/versions/$phpversionname/etc/php-fpm.d/www.conf.default ]; then
-	cp /home/$USER/.phpenv/versions/$phpversionname/etc/php-fpm.d/www.conf.default /home/$USER/.phpenv/versions/$phpversionname/etc/php-fpm.d/www.conf
-	file=/home/$USER/.phpenv/versions/$phpversionname/etc/php-fpm.d/www.conf
-fi;
-sed -e "s,listen = 127.0.0.1:9000,listen = /tmp/php${phpversionname:0:1}-fpm.sock,g" --in-place $file
-sed -e "s,;listen.owner = nobody,listen.owner = $USER,g" --in-place $file
-sed -e "s,;listen.group = nobody,listen.group = $USER,g" --in-place $file
-sed -e "s,;listen.mode = 0660,listen.mode = 0666,g" --in-place $file
-sed -e "s,user = nobody,;user = $USER,g" --in-place $file
-sed -e "s,group = nobody,;group = $USER,g" --in-place $file
+sudo apt-get install -y --force-yes apache2 libapache2-mod-fastcgi > /dev/null
+sudo mkdir $(pwd)/.run
+chmod a+x tests/travis-php-fpm.sh
+sudo ./tests/travis-php-fpm.sh $USER $(phpenv version-name)
 sudo a2enmod rewrite actions fastcgi alias
-echo "cgi.fix_pathinfo = 1" >> ~/.phpenv/versions/$phpversionname/etc/php.ini
+echo "cgi.fix_pathinfo = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 ~/.phpenv/versions/$(phpenv version-name)/sbin/php-fpm
-sudo cp -f ./tests/travis-ci-apache.conf /etc/apache2/sites-available/default.conf
-sudo sed -e "s?%TRAVIS_BUILD_DIR%?$(pwd)?g" --in-place /etc/apache2/sites-available/default.conf
-sudo sed -e "s?%PHPVERSION%?${TRAVIS_PHP_VERSION:0:1}?g" --in-place /etc/apache2/sites-available/default.conf
-sudo a2ensite default.conf
+sudo cp -f tests/travis-ci-apache.conf /etc/apache2/sites-available/default
+sudo sed -e "s?%TRAVIS_BUILD_DIR%?$(pwd)?g" --in-place /etc/apache2/sites-available/default
+sudo sed -e "s?%PHPVERSION%?${TRAVIS_PHP_VERSION:0:1}?g" --in-place /etc/apache2/sites-available/default
 sudo service apache2 restart
-
-composer install
-
-#npm install
-
-sh -e /etc/init.d/xvfb start
-#fluxbox &
-#sleep 3
+"export DISPLAY=:99.0"
+"sh -e /etc/init.d/xvfb start"
+sleep 3 # give xvfb some time to start
+sudo apt-get install fluxbox -y --force-yes
+fluxbox &
+sleep 3 # give fluxbox some time to start
