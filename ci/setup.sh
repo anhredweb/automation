65e6f2cfc455f43931e7936c097173c9fcaf438c
tuvuong@asiantechhub.com <tuvuong@asiantechhub.com>
2018-04-09 16:16:22 +0700

    update code

--- ci/setup.sh
+++ ci/setup.sh
@@ -65,12 +65,12 @@ else
 	unzip ~/chromedriver_linux64.zip -d ~/
 	rm ~/chromedriver_linux64.zip
 	sudo mv -f ~/chromedriver /usr/local/bin/chromedriver
-	sudo chown root:root /usr/local/bin/chromedriver
+	#sudo chown root:root /usr/local/bin/chromedriver
 	sudo chmod 0755 /usr/local/bin/chromedriver
 
 	# Install Selenium.
 	wget -N http://selenium-release.storage.googleapis.com/$SELENIUM_SUBDIR/selenium-server-standalone-$SELENIUM_STANDALONE_VERSION.jar -P ~/
 	sudo mv -f ~/selenium-server-standalone-$SELENIUM_STANDALONE_VERSION.jar /usr/local/bin/selenium-server-standalone.jar
-	sudo chown root:root /usr/local/bin/selenium-server-standalone.jar
+	#sudo chown root:root /usr/local/bin/selenium-server-standalone.jar
 	sudo chmod 0755 /usr/local/bin/selenium-server-standalone.jar
 fi
