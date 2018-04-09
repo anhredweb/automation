7b850393d2d49829e64dd693b20ec886a9040b4a
tuvuong@asiantechhub.com <tuvuong@asiantechhub.com>
2018-04-09 18:07:59 +0700

    update code

--- ci/setup.sh
+++ ci/setup.sh
@@ -36,5 +36,6 @@ sleep 3
 sudo apt-get install fluxbox -y --force-yes
 fluxbox &
 sleep 3
+export DISPLAY=:99.0
 java -jar -Dwebdriver.chrome.driver="chrome_driver/chromedriver" "selenium-server-standalone.jar"
 sleep 3
