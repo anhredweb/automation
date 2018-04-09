079a53a54516ffb5a0928ca26568b6d0e5356b4b
tuvuong@asiantechhub.com <tuvuong@asiantechhub.com>
2018-04-09 16:09:30 +0700

    update code

--- ci/setup.sh
+++ ci/setup.sh
@@ -53,4 +53,24 @@ else
 
 	sudo apt-get install oracle-java8-installer
 	sudo apt-get install oracle-java8-set-default
+
+	# Install Chrome.
+	sudo curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add
+	sudo echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
+	sudo apt-get -y update
+	sudo apt-get -y install google-chrome-stable
+
+	# Install ChromeDriver.
+	wget -N http://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip -P ~/
+	unzip ~/chromedriver_linux64.zip -d ~/
+	rm ~/chromedriver_linux64.zip
+	sudo mv -f ~/chromedriver /usr/local/bin/chromedriver
+	sudo chown root:root /usr/local/bin/chromedriver
+	sudo chmod 0755 /usr/local/bin/chromedriver
+
+	# Install Selenium.
+	wget -N http://selenium-release.storage.googleapis.com/$SELENIUM_SUBDIR/selenium-server-standalone-$SELENIUM_STANDALONE_VERSION.jar -P ~/
+	sudo mv -f ~/selenium-server-standalone-$SELENIUM_STANDALONE_VERSION.jar /usr/local/bin/selenium-server-standalone.jar
+	sudo chown root:root /usr/local/bin/selenium-server-standalone.jar
+	sudo chmod 0755 /usr/local/bin/selenium-server-standalone.jar
 fi
