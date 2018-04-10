2a18809dc4a127a407c3341c33c62b37cab8764e
tuvuong@asiantechhub.com <tuvuong@asiantechhub.com>
2018-04-10 09:37:22 +0700

    update code

--- ci/setup.sh
+++ ci/setup.sh
@@ -7,9 +7,9 @@ sudo apt-get update -qq
 #sudo apt-get install --yes --force-yes apache2 libapache2-mod-fastcgi oracle-java8-installer oracle-java8-set-default
 sudo wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.7-linux-x86_64.tar.bz2
 sudo tar xjf phantomjs-1.9.7-linux-x86_64.tar.bz2
-sudo ln -s /usr/local/share/phantomjs-1.9.7-linux-x86_64/bin/phantomjs /usr/local/share/phantomjs
-sudo ln -s /usr/local/share/phantomjs-1.9.7-linux-x86_64/bin/phantomjs /usr/local/bin/phantomjs
-sudo ln -s /usr/local/share/phantomjs-1.9.7-linux-x86_64/bin/phantomjs /usr/bin/phantomjs
+sudo ln -s /phantomjs-1.9.7-linux-x86_64/bin/phantomjs /phantomjs
+sudo ln -s /phantomjs-1.9.7-linux-x86_64/bin/phantomjs /phantomjs
+sudo ln -s /phantomjs-1.9.7-linux-x86_64/bin/phantomjs /phantomjs
 
 composer install
 
