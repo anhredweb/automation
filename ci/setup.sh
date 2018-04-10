4c05fc48c2d900c63281a95fd6d5ebf96baef71a
tuvuong@asiantechhub.com <tuvuong@asiantechhub.com>
2018-04-10 13:53:45 +0700

    update code

--- ci/setup.sh
+++ ci/setup.sh
@@ -3,8 +3,11 @@ set -ev
 
 sudo apt-get update -qq
 
-composer require se/selenium-server-standalone
-composer install
-
 sh -e /etc/init.d/xvfb start
 sleep 3
+sudo apt-get install fluxbox -y --force-yes
+fluxbox &
+sleep 3
+
+composer require se/selenium-server-standalone
+composer install
